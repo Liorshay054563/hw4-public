a487639bd83e17d4a327b99d426b66f1a4dfe493
Liorshay054563 <liorshay054563@gmail.com>
2024-09-11 10:18:09 +0300

    Add files via upload

---
+++ hw4/hw4(4).py
@@ -0,0 +1,29 @@
+# start x+1
+
+x: int = 10
+
+while x <= 20:
+    print(x)
+    x = x + 1
+# end
+
+# start x+2
+
+y: int = 10
+
+while y <= 20:
+    print(y)
+    y = y + 2
+
+# end
+
+# start input gap
+
+a: int = 10
+z: int = int(input('please enter the gap:'))
+
+while y <= 20:
+    print(y)
+    y = y + z
+
+# end
